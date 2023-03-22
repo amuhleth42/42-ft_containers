bb1d8f938ec398c41f3790b2642ce37c6f188cfc
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2023-03-22 20:49:16 +0100

    vector relational operators done, vector implementation completed :]

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -347,25 +347,46 @@ public:
 };
 
 template< class T, class Alloc>
-bool	operator==(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+bool	operator==(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+{
+	return (lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin()));
+}
 
 template< class T, class Alloc>
-bool	operator!=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+bool	operator!=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+{
+	return !(lhs == rhs);
+}
 
 template< class T, class Alloc>
-bool	operator<(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+bool	operator<(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+{
+	return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
+}
 
 template< class T, class Alloc>
-bool	operator<=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+bool	operator<=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+{
+	return !(rhs < lhs);
+}
 
 template< class T, class Alloc>
-bool	operator>(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+bool	operator>(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+{
+	return rhs < lhs;
+}
 
 template< class T, class Alloc>
-bool	operator>=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+bool	operator>=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+{
+	return !(lhs < rhs);
+}
 
 template< class T, class Alloc>
-void	swap(vector<T, Alloc>& x, vector<T, Alloc>& y);
+void	swap(vector<T, Alloc>& x, vector<T, Alloc>& y)
+{
+	x.swap(y);
+}
 
 }
 #endif
