e535efbb167654ff52acf116903ff9eeba885597
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2023-03-24 00:03:18 +0100

    proto rbtree

--- srcs/RBTree.hpp
+++ srcs/RBTree.hpp
@@ -39,7 +39,15 @@ public:
 	reference	operator*(void) const	{ return content; }
 	pointer		operator->(void) const	{ return &content; }
 
+	// todo ?
+	
+	bool	operator==(const RBNode& rhs);
+	bool	operator!=(const RBNode& rhs);
 
+	RBNode*	mini(void);
+	RBNode*	maxi(void);
+	RBNode*	previous(void);
+	RBNode*	next(void);
 
 };
 
@@ -48,6 +56,40 @@ public:
 template< class T, class Compare = ft::less<T>, class Alloc = std::allocator<T> >
 class	RBTree
 {
+
+private:
+	node_ptr	newNode(const_reference content);
+	node_ptr	getRoot() const;
+	
+	void	leftRotate(node_ptr n);
+	void	rightRotate(node_ptr n);
+
+	void	insertFix(note_ptr newNode);
+	void	deleteFix(node_ptr x);
+
+	void	transplant(node_ptr x, node_ptr y);
+
+	void	clearNode(RBNodeAllocator& alloc, node_ptr n) const;
+
+	node_ptr	_getNode(const_reference k) const;
+
+	iterator	insertNode(const_reference k, node_ptr hint = NULL);
+
+	int	deleteNode(const node_ptr toDelete);
+
+public:
+	RBTree();
+
+	explicit RBTree(...);
+
+	template< class InputIterator >
+	RBTree(...);
+
+	RBTree(const RBTree& src);
+
+	~RBTree();
+
+	RBTree&	operator=(const RBTree& rhs);
 
 
 
